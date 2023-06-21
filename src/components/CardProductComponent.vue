42b582364bda2c248b658b192cf348c7e3f9e23e
anandaaputrap <anandapratama4869@gmail.com>
2023-06-21 15:51:36 +0700

    axioslongor

---
+++ src/components/CardProductComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Makanan</h2>
+</template>
+
+<script>
+export default {
+    name: 'CardProductComponent',
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,20 +3,57 @@
     <NavbarComponent />
     <div class="container">
       <BannerComponent />
+      <div class="row mt-4">
+        <div class="col">
+          <h2>Makanan <strong>Terbaik</strong></h2>
+        </div>
+        <div class="col">
+          <router-link class="btn btn-success float-right" to="/makanan">
+            Lihat Semua Makanan
+          </router-link>
+        </div>
+      </div>
+
+      <div class="row mb-3">
+        <div class="col-md-3 mt-4">
+          <CardProductComponent />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import NavbarComponent from '@/components/NavbarComponent.vue'
-import BannerComponent from '@/components/BannerComponent.vue'
+import NavbarComponent from '@/components/NavbarComponent.vue';
+import BannerComponent from '@/components/BannerComponent.vue';
+import CardProductComponent from '@/components/CardProductComponent.vue';
+import axios from "axios";
 
 export default {
   name: 'HomeView',
   components: {
     NavbarComponent,
     BannerComponent,
-  }
+    CardProductComponent,
+  },
+  data () {
+    return {
+      products: [
+       
+      ]
+    }
+  },
+  methods: {
+    setProducts(data) {
+      this.products = data;
+    }
+  },
+  mounted() {
+    axios
+      .get("http://localhost:3000/best-products")
+      .then((response) => this.setProducts(response.data))
+      .catch((error) => console.log(error))
+  },
 }
 </script>
